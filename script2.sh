88d94d6b62d5223c015e107d18839604f3e8f15f
Gautham-kukutla <36068420+Gautham-kukutla@users.noreply.github.com>
2022-03-14 19:48:53 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -3,8 +3,9 @@ echo "entered shell script"
 if [ -e "changes.txt" ]
 then
   echo "changes done"
-  #git push 'https://github.com/Gautham-kukutla/pythongit2.git'
-git push origin main
+ #git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/Gautham-kukutla/pythongit2.git
+ #git push 'https://github.com/Gautham-kukutla/pythongit2.git'
+#git push origin main
 #git push 'git@github.com:Gautham-kukutla/pythongit2.git'
 else
 echo "no changes"
