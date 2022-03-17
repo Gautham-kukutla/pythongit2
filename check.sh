6e08f6aab2c190f32229572f0949a2b8a2de0d84
Gautham-kukutla <36068420+Gautham-kukutla@users.noreply.github.com>
2022-03-17 23:53:04 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -3,3 +3,6 @@ if [ $(dpkg-query -W -f='${Status}' python3 2>/dev/null | grep -c "ok installed"
 then
   apt-get install python3 ;
 fi
+if [ $(pip list | grep -c "GitPython") -eq 0 ]
+then pip install GitPython
+fi
